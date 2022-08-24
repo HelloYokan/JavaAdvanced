859052da1c5cb37d1a9605f76d4407a76c95b023
Yezhov Oleksandr <bladeagainst@gmail.com>
2022-08-24 22:58:49 +0300

    Hometask 1

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainTest {
+
+}
